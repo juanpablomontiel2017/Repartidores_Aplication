ed8ae5d47fe0d801a88d92b84f410b51058495b9
Juan Pablo <juanpablomontiel2015@gmail.com>
2018-06-20 19:10:27 -0300

    finish
    Ultimas modificaciones al fragment_ventas

--- app/src/main/java/com/example/jumpi/repartidores_aplication/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/jumpi/repartidores_aplication/RecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package com.example.jumpi.repartidores_aplication;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
