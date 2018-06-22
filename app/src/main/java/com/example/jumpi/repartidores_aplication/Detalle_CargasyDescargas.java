857d836622661f072ac1284fc6ce66638003cca7
Juan Pablo <juanpablomontiel2015@gmail.com>
2018-06-22 20:23:35 -0300

    ACTIVITY_DETALLE_CARGAS_DESCARGAS
    Se termino con la activity detalle de cargas y descargas

---
+++ app/src/main/java/com/example/jumpi/repartidores_aplication/Detalle_CargasyDescargas.java
@@ -0,0 +1,13 @@
+package com.example.jumpi.repartidores_aplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detalle_CargasyDescargas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle__cargasy_descargas);
+    }
+}
