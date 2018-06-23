dcfdf7178f3396db40eb1e556cf3b4c5612cea36
Matias Maldonado <matiasmaldonado_ch@hotmail.com>
2018-06-23 12:46:05 -0300

    Test FUNCIONA
    Modifique manifest para acceso a internet

--- app/src/main/java/com/example/jumpi/repartidores_aplication/LoginRequest.java
+++ app/src/main/java/com/example/jumpi/repartidores_aplication/LoginRequest.java
@@ -3,14 +3,15 @@ package com.example.jumpi.repartidores_aplication;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class LoginRequest extends StringRequest {
-
+// "http://192.168.1.14/av/login/login.php"
+// "https://aquavital.000webhostapp.com/login/login.php"
     private static final String LOGIN_REQUEST_URL = "https://aquavital.000webhostapp.com/login/login.php";
     private Map<String, String> params;
 
