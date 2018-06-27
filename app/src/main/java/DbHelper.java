59c235c4653ae571045fe012106f3b560d5de3b5
Matias Maldonado <matiasmaldonado_ch@hotmail.com>
2018-06-27 18:04:31 -0300

    nuevos métodos para usar la DB
    creé métodos para guardar, leer y actualizar la tabla repartidor

--- app/src/main/java/DbHelper.java
+++ app/src/main/java/DbHelper.java
@@ -1,3 +1,6 @@
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -6,16 +9,51 @@ import com.example.jumpi.repartidores_aplication.DbContract;
 public class DbHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
-    private static final String CREATE_TABLE = "create table "+DbContract.TABLE_NAME+"("+DbContract.DNI+" integer primary key,"+DbContract.IDREPARTIDOR+" integer,"+DbContract.USUARIO+" text,"+DbContract.PASSWORD+" text);";
+    private static final String CREATE_TABLE = "create table "+DbContract.TABLE_NAME+"("+DbContract.DNI+" integer primary key,"+DbContract.IDREPARTIDOR+" integer,"+DbContract.USUARIO+" text,"+DbContract.PASSWORD+" text, "+DbContract.SYNC_STATUS+" integer);";
     private static final String DROP_TABLE = "drop table if exists "+DbContract.TABLE_NAME;
-    
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
+    public DbHelper (Context context){
+        super(context, DbContract.DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL(DROP_TABLE);
+        onCreate(db);
+    }
+
+    public  void saveToLocalDatabase(int dni, int idrepartidor, String usuario, String password, int sync_status, SQLiteDatabase database){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DbContract.DNI, dni);
+        contentValues.put(DbContract.IDREPARTIDOR, idrepartidor);
+        contentValues.put(DbContract.USUARIO, usuario);
+        contentValues.put(DbContract.PASSWORD, password);
+        contentValues.put(DbContract.SYNC_STATUS, sync_status);
+        database.insert(DbContract.TABLE_NAME,null,contentValues);
 
     }
+
+    public Cursor readFromLocalDatabase (SQLiteDatabase database){
+        String[] projection = {DbContract.DNI, DbContract.IDREPARTIDOR, DbContract.USUARIO, DbContract.PASSWORD, DbContract.SYNC_STATUS};
+
+        return (database.query(DbContract.TABLE_NAME,projection, null,null, null,null,null));
+    }
+
+    public void updateLocalDatabase(int dni, int idrepartidor, String usuario, String password, int sync_status, SQLiteDatabase database){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DbContract.USUARIO, usuario);
+        contentValues.put(DbContract.PASSWORD, password);
+        contentValues.put(DbContract.SYNC_STATUS, sync_status);
+
+        String selection = DbContract.DNI+" = ";
+        String[] selection_args = {Integer.toString(dni)};
+        database.update(DbContract.TABLE_NAME,contentValues,selection,selection_args);
+    }
+
+
 }
