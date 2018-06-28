2f0f66aa94a2e852b0dee3848792efc9ceeefcf5
Matias Maldonado <matiasmaldonado_ch@hotmail.com>
2018-06-28 14:29:06 -0300

    Revert "ENPROCESO"
    This reverts commit db33fe6b8141fcd5570e530739dead05f0046566.

--- app/src/main/java/com/example/jumpi/repartidores_aplication/DbContract.java
+++ app/src/main/java/com/example/jumpi/repartidores_aplication/DbContract.java
@@ -6,17 +6,12 @@ public class DbContract {
     public static final int SYNC_STATUS_FAILED = 1;
 
     public static final String DATABASE_NAME = "aquavitaldb";
-
-    public static final String TABLE_NAME_USUARIO = "usuario";
-    public static final String ID = "id";
+    public static final String TABLE_NAME = "repartidor";
+    public static final String IDREPARTIDOR = "idrepartidor";
     public static final String DNI = "dni";
     public static final String USUARIO = "usuario";
     public static final String PASSWORD = "contrasena";
     public static final String SYNC_STATUS = "syncstatus";
-
-
-
-
 
 
 }
