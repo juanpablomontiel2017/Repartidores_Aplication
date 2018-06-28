b5ee2f6bbd32281ffac155b3da771ea6b80217a6
Juan Pablo <juanpablomontiel2015@gmail.com>
2018-06-27 21:27:30 -0300

    listo el eliminar
    adadasdasasds

--- app/src/main/java/com/example/jumpi/repartidores_aplication/Dialog_Eliminar_Clientes.java
+++ app/src/main/java/com/example/jumpi/repartidores_aplication/Dialog_Eliminar_Clientes.java
@@ -1,7 +1,10 @@
 package com.example.jumpi.repartidores_aplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dialog_Eliminar_Clientes extends AppCompatActivity {
 
@@ -10,6 +13,17 @@ public class Dialog_Eliminar_Clientes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog__eliminar__clientes);
 
+        final Button buttonCancelar = (Button) findViewById(R.id.btn_cancelar);
 
+        buttonCancelar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent buttonCancel = new Intent(Dialog_Eliminar_Clientes.this, Second_Activity.class);
+                startActivity(buttonCancel);
+
+            }
+        });
     }
 }
