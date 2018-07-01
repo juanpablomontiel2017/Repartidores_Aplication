f5ec937db73dbbcbab40948cd333a40d5db2394d
Matias Maldonado <matiasmaldonado_ch@hotmail.com>
2018-07-01 18:28:59 -0300

    ENPROCESO
    ConnectionStateMonitor
    es una clase que me permite ver el estado de la conexión.
    
    ConnectivityReceiver y NetworkSchedulerService
    son un servicio que me ayuda a ver el estado de la conexión pero necesito iniciarlo desde el manifest para que funcione. POR AHORA ESTA DESACTIVADO

--- app/src/main/java/com/example/jumpi/repartidores_aplication/MainActivity.java
+++ app/src/main/java/com/example/jumpi/repartidores_aplication/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.jumpi.repartidores_aplication;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //scheduleJob();
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -160,5 +165,37 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+
+    private void scheduleJob() {
+        JobInfo myJob = new JobInfo.Builder(0, new ComponentName(this, NetworkSchedulerService.class))
+                .setRequiresCharging(true)
+                .setMinimumLatency(1000)
+                .setOverrideDeadline(2000)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .setPersisted(true)
+                .build();
+
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.schedule(myJob);
+    }
+
+    protected void onStop() {
+        // A service can be "started" and/or "bound". In this case, it's "started" by this Activity
+        // and "bound" to the JobScheduler (also called "Scheduled" by the JobScheduler). This call
+        // to stopService() won't prevent scheduled jobs to be processed. However, failing
+        // to call stopService() would keep it alive indefinitely.
+        stopService(new Intent(this, NetworkSchedulerService.class));
+        super.onStop();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        // Start service and provide it a way to communicate with this class.
+        Intent startServiceIntent = new Intent(this, NetworkSchedulerService.class);
+        startService(startServiceIntent);
+    }
+
+     */
 
 }
