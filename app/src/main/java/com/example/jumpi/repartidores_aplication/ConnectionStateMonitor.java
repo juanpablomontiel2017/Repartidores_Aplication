cc13cd5b6b94522f04abf0f718f4b71879ff7de5
Matias Maldonado <matiasmaldonado_ch@hotmail.com>
2018-07-01 16:46:19 -0300

    ENPROCESO

--- app/src/main/java/com/example/jumpi/repartidores_aplication/ConnectionStateMonitor.java
+++
@@ -1,8 +0,0 @@
-package com.example.jumpi.repartidores_aplication;
-
-import android.net.ConnectivityManager;
-
-public class ConnectionStateMonitor extends ConnectivityManager.NetworkCallback {
-
-
-}
