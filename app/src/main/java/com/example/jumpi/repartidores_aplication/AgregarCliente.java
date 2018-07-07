bd6fdda9832609ae01c9ec1c3bfbb60e6a77977d
Juan Pablo <juanpablomontiel2015@gmail.com>
2018-07-07 20:50:52 -0300

    checkbox en agregarCliente.java
    dasdsa

--- app/src/main/java/com/example/jumpi/repartidores_aplication/AgregarCliente.java
+++ app/src/main/java/com/example/jumpi/repartidores_aplication/AgregarCliente.java
@@ -5,13 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class AgregarCliente extends AppCompatActivity {
+
+    //Declaración de variables para cada CheckBox correspondiente a los 6 días laborales
+
+    CheckBox c1, c2, c3, c4, c5, c6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_cliente);
+
 
 
         final Button buttonCancelar = (Button) findViewById(R.id.button_cancelar_AC);
@@ -43,5 +51,36 @@ public class AgregarCliente extends AppCompatActivity {
         });
 
 
+        c1 = (CheckBox) findViewById(R.id.idcheckbox_lunes);
+        c2 = (CheckBox) findViewById(R.id.idcheckbox_martes);
+        c3 = (CheckBox) findViewById(R.id.idcheckbox_miercoles);
+        c4 = (CheckBox) findViewById(R.id.idcheckbox_jueves);
+        c5 = (CheckBox) findViewById(R.id.idcheckbox_viernes);
+        c6 = (CheckBox) findViewById(R.id.idcheckbox_sabado);
+
+
     }
+
+  public void onclick (View view) {
+
+        if (view.getId()==R.id.button_agregar_AC) {
+
+            MethodAgregarC();
+        } //Fin de la sentencia 'if'
+
+  }
+
+
+    private void MethodAgregarC(){
+
+
+       
+
+    } //Fin del método
+
+
+
+
+
+
 }
